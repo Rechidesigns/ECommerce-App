ae9fe559361da7725d78bd1edfe9173dc219fcb5
Rechidesigns <alerechiordu@gmail.com>
2022-10-12 19:29:47 +0100

    debug= false

--- projectfolder/settings.py
+++ projectfolder/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ecommerceapiapp.herokuapp.com', '127.0.0.1']
 
